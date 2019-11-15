c7c708fd8a0092b0872f64ea9d4eb7ec412e6be1
fgiuste <fgiuste@emory.edu>
2019-11-14 21:21:44 -0500

    Fast Pipeline update

--- Fast_Pipeline.sh
+++ Fast_Pipeline.sh
@@ -3,11 +3,16 @@
 export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/usr/lib/ROBEX/elastix
 
 subject_path=$1
-
+echo
+echo "Running Step 1 and 2 for subject: ${subject_path}"
 bash epi_step1.sh ${subject_path} &
 bash anat_step2.sh ${subject_path} &
 
 wait
+echo
+echo "Running Step 3 for subject: ${subject_path}"
 bash anat_func_step3.sh ${subject_path}
+
+echo "Finished Successfully!"
 
 
